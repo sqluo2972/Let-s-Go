62f009639b211a558aacede234a510f7123f3024
sqluo2972 <c0096321@gmail.com>
2024-08-17 18:34:52 +0800

    ch3-5 Isolating Route

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,22 +31,10 @@ func main() {
 		infoLog:  infolog,
 	}
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/".
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorlog,
-		Handler:  mux,
+		Handler:  app.route(),
 	}
 
 	infolog.Printf("Starting server on %s", *addr)
