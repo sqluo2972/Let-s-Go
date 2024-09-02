0e7622bc56701cd67e492c6d33c330bb9f47460e
AlanLo <alanlo@AlanLodeMacBook-Air.local>
2024-09-02 16:27:43 +0800

    6-4 panic recovery

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app application) route() *http.ServeMux {
+func (app *application) route() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -12,5 +12,5 @@ func (app application) route() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.recoverPanic(app.logRequest(secureHeader(mux)))
 }
