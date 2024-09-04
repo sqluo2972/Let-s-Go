178c1e0c077fb167746d4437528b54e2093ecfa1
AlanLo <alanlo@AlanLodeMacBook-Air.local>
2024-09-04 15:10:01 +0800

    11-1 user login display

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,6 +25,12 @@ func (app *application) route() http.Handler {
 	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	standard := alice.New(app.recoverPanic, app.recoverPanic, app.logRequest)
 
 	return standard.Then(router)
