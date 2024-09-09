364c659e13a0a02d10939fe89c5b0b325daa2d7e
AlanLo <alanlo@AlanLodeMacBook-Air.local>
2024-09-09 14:41:29 +0800

    14-5 dependence mocking

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"snippetbox.alan/internal/assert"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a mock HTTP handler that we can pass to our secureHeaders
+	// middleware, which writes a 200 status code and an "OK" response body.
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	// Pass the mock HTTP handler to our secureHeaders middleware. Because
+	// secureHeaders *returns* a http.Handler we can call its ServeHTTP()
+	// method, passing in the http.ResponseRecorder and dummy http.Request to
+	// execute it.
+	secureHeader(next).ServeHTTP(rr, r)
+
+	// Call the Result() method on the http.ResponseRecorder to get the results
+	// of the test.
+	rs := rr.Result()
+
+	expectedValue := "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com"
+	assert.Equal(t, rs.Header.Get("Content-Security-Policy"), expectedValue)
+
+	expectedValue = "origin-when-cross-origin"
+	assert.Equal(t, rs.Header.Get("Referrer-Policy"), expectedValue)
+
+	expectedValue = "nosniff"
+	assert.Equal(t, rs.Header.Get("X-Content-Type-Options"), expectedValue)
+
+	expectedValue = "deny"
+	assert.Equal(t, rs.Header.Get("X-Frame-Options"), expectedValue)
+
+	expectedValue = "0"
+	assert.Equal(t, rs.Header.Get("X-XSS-Protection"), expectedValue)
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, string(body), "OK")
+
+}
