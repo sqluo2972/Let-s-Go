c69de06b0b5cc3214d8738c1c96c7623ddfbb402
AlanLo <alanlo@AlanLodeMacBook-Air.local>
2024-09-09 15:38:09 +0800

    finished

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 	"time"
 
@@ -71,4 +73,14 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	body, err := io.ReadAll(rs.Body)
 
 	return rs.StatusCode, rs.Header, string(body)
+}
+
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	return html.UnescapeString(string(matches[1]))
 }
